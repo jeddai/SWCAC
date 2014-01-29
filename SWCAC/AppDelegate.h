4b3947240aadbcc32ffab3a152cedf622f68ba51
Charles Humphreys <Charles@charless-mbp.lipscomb.edu>
2014-01-29 14:38:38 -0600

    Initial Commit

---
+++ SWCAC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SWCAC
+//
+//  Created by Charles Humphreys on 1/29/14.
+//  Copyright (c) 2014 Charles Humphreys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SWCAC/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  SWCAC
+//
+//  Created by Charles Humphreys on 1/29/14.
+//  Copyright (c) 2014 Charles Humphreys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
