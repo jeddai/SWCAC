7640350b6577d7c5177ee19a78bf2e629996fe55
jeddai <cahumphreys@mail.lipscomb.edu>
2014-05-15 12:07:33 -0500

    Update
    05.15.2014

---
+++ SWCAC/ModalViewController.h
@@ -0,0 +1,14 @@
+//
+//  ModalViewController.h
+//  SWCAC
+//
+//  Created by Charles Humphreys on 5/15/14.
+//  Copyright (c) 2014 Charles Humphreys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+- (IBAction)Close:(id)sender;
+
+@end
